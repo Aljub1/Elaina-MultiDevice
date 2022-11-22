8cb3d6e82530ccc4c1c142b6fc93ab3738f43d44
Aljub1 <118148515+Aljub1@users.noreply.github.com>
2022-11-22 14:37:27 +0700

    config.js

--- config.js
+++ config.js
@@ -54,15 +54,15 @@ global.sdc = '-' //Isi Pake Link Discordmu Kalo Gada Biarin aja
 global.snh = 'https://nhentai.net/g/HaramTod🗿' //Hmmm, Ini boleh kalian isi terserah :v
 
 /*============== PAYMENT ==============*/
-global.pdana = '085736178354' //Nomor Danamu
-global.ppulsa = '085736178354' //Nomor SimCard Yang Kamu Pake
+global.pdana = '083847871545' //Nomor Danamu
+global.ppulsa = '083847871545' //Nomor SimCard Yang Kamu Pake
 global.psaweria = 'https://saweria.com/XiaoYan021' //Link Saweriamu Kalo Nggada Ketik - aja
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6281231079387' //Nomor Bot
-global.nomorown = '6285736178354' //Nomor Owner
-global.namebot = 'Elaina BOT' //Nama Bot
-global.nameown = '𝓧𝓲𝓪𝓸 - 𝓨𝓪𝓷' // Nama Owner
+global.nomorbot = '6283847871545' //Nomor Bot
+global.nomorown = '6283847871545' //Nomor Owner
+global.namebot = 'aljub BOT' //Nama Bot
+global.nameown = 'aljub' // Nama Owner
 
 
 /*============== STAFF ==============*/
